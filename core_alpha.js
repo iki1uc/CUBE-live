3f4a45c862f84f69538350c7e670103ee50328e3
iki1uc <entertrainer@web.de>
2026-05-09 23:23:49 +0200

    Update core_alpha.js

--- core_alpha.js
+++ core_alpha.js
@@ -1,1 +1,11 @@
 
+console.log("core_alpha geladen – Cube-Motor aktiv.");
+
+// Cube 0 Input-Pol
+function cube0Input(value) {
+  const pol = document.querySelector(".cube0 .input-pol");
+  if (!pol) return;
+
+  pol.textContent = value;
+  pol.style.boxShadow = "0 0 14px #00eaff, inset 0 0 6px #000";
+}
