e5d60dcf276318cc1a39a5e2d84ce8d2821928a9
iki1uc <entertrainer@web.de>
2026-05-17 01:27:28 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
 
+// /api/server.js
+
+import express from "express";
+import lvlup from "./lvlup.js";
+
+const app = express();
+
+app.get("/api/lvlup", lvlup);
+
+app.listen(3000, () => {
+  console.log("API läuft auf Port 3000");
+});
