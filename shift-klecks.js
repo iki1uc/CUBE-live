f0fe8967f17b7c08b4c2158b2367c5bb9e2e5798
iki1uc <entertrainer@web.de>
2026-05-19 20:26:34 +0200

    Update shift-klecks.js

--- shift-klecks.js
+++ shift-klecks.js
@@ -1,1 +1,20 @@
 
+# DE: SHIFT-KLECKS
+// Aus Tropfen wird Struktur.
+// Aus Struktur wird Linie.
+// Aus Linie wird Schrift.
+
+# FR: SHIFT-KLECKS
+// Des gouttes deviennent structure.
+// La structure devient ligne.
+// La ligne devient écriture.
+
+# DE
+// Das ist der Moment des Wandels.
+// Der SHIFT.
+// Aus wenig wird viel.
+
+# FR
+// C’est le moment du changement.
+// Le SHIFT.
+// Du peu devient beaucoup.
