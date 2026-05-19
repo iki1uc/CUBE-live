205c38b6daf5e9829735ce3ce39ca83200377d25
iki1uc <entertrainer@web.de>
2026-05-19 20:26:16 +0200

    Update klecks-splash.js

--- klecks-splash.js
+++ klecks-splash.js
@@ -1,1 +1,18 @@
+# DE: KLECKS-SPLASH
+// Der Beutel platzt.
+// Farbe springt.
+// Der Bildschirm bekommt Leben.
+
+# FR: ÉCLABOUSSURE KLECKS
+// Le sac éclate.
+// La couleur saute.
+// L’écran respire enfin.
+
+# DE
+// Spritzer landen zufällig.
+// Doch die Lücken erzählen die Wahrheit.
+
+# FR
+// Les éclats tombent au hasard.
+// Mais les vides racontent la vérité.
 
